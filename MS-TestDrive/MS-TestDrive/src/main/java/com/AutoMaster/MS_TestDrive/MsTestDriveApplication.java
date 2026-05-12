aa65c613d4fd1f2fdb25f8024771afe38ce67c75
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:43:04 -0400

    Add files via upload

---
+++ MS-TestDrive/MS-TestDrive/src/main/java/com/AutoMaster/MS_TestDrive/MsTestDriveApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_TestDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTestDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTestDriveApplication.class, args);
+	}
+
+}
---
+++ MS-TestDrive/MS-TestDrive/src/test/java/com/AutoMaster/MS_TestDrive/MsTestDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_TestDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTestDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
